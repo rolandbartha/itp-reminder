5c53bab18e3bf69f840a881c42d0c53d8263a107
Roland Bartha <markmarko2001@gmail.com>
2025-09-06 18:26:38 +0300

    added tests & removed sql coonnection requirement

--- web/src/test/java/net/rolibrt/itp_reminder/ItpReminderApplicationTests.java
+++ web/src/test/java/net/rolibrt/itp_reminder/ItpReminderApplicationTests.java
@@ -1,13 +1,37 @@
 package net.rolibrt.itp_reminder;
 
+import net.rolibrt.itp_reminder.models.WebUser;
+import net.rolibrt.itp_reminder.repositories.WebUserRepository;
+import net.rolibrt.itp_reminder.services.WebUserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.Mockito;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@ActiveProfiles("test")
 class ItpReminderApplicationTests {
+    private WebUserService userService;
+    private WebUserRepository userRepository;
 
-	@Test
-	void contextLoads() {
-	}
+    @BeforeEach
+    void setup() {
+        userRepository = Mockito.mock(WebUserRepository.class);
+        userService = new WebUserService(userRepository);
+    }
 
+    @Test
+    void testCreateAndGetUser() {
+        WebUser user = new WebUser();
+        user.setUsername("Bob");
+        user.setEmail("bob@mail.com");
+        Mockito.when(userRepository.findByEmail("bob@mail.com"))
+                .thenReturn(Optional.of(user));
+
+        WebUser result = userService.findByEmail("bob@mail.com").get();
+        assertEquals("Bob", result.getUsername());
+    }
 }
